d5c64137335a49e97dff53126db4f07a24f70e13
Igor MIranda <igor_midev@outlook.com>
2023-02-25 23:48:51 -0300

    Changelog updated

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,14 +1,27 @@
-## 1.0.4
+## [2.0.0] - Big Update
+- **BREAKING:** Changed way of logging, using a builder pattern
+- Readme update
+- 100% test coverage in all usecases
+- Implemented the builder pattern
+- New arts
+- Added feature to:
+    - Log map and list of maps
+    - Log objects that have expected `toString()` pattern
+    - Log error stacktrace
+    - Change color as a dynamic way
+- Assert in map log bug
+
+## [1.0.4]
 - Readme is better now.
 
-## 1.0.3
+## [1.0.3]
 - `print` has changed to `log`.
 
-## 1.0.2
+## [1.0.2]
 - Example changed to be more didactic.
 
-## 1.0.1
+## [1.0.1]
 - Documentation change.
 
-## 1.0.0 
+## [1.0.0]
 - Initial version.
--- example/sandbox_logger_example.dart
+++ example/sandbox_logger_example.dart
@@ -37,7 +37,7 @@ Future<void> main() async {
   final person1 = Person('Igor', 22, true);
   final person2 = Person('Daniel', 38, true);
 
-  SandLog.object(person1); // Log 1 object
+  SandLog.object(user); // Log 1 object
 
   SandLog.listOfObject([person1, person2]); // Log a list of object
 }
@@ -66,6 +66,152 @@ final fakeStackText = '''
 ''';
 
 final testComplexMap = {
+  'userName': 'Igor',
+  'age': 22,
+  'totalCash': 138.57,
+  'isMale:': true,
+  'userStatus': {
+    'isUserActive': true,
+    'isAPremiumUser': false,
+  },
+  'userTags': ['Flutter Developer', 'Proactive', 'Clean code'],
+  'userCourses': [
+    {
+      'title': 'How to use TDD in flutter',
+      'finished': false,
+      'coursePercentage': 47.4,
+      'buyDate': '1969-07-20T20:18:04.000Z',
+      'courseTag': ['New', 'Popular'],
+      'courseDetails': null,
+    },
+    {
+      'title': 'Clean arquiteture in flutter',
+      'finished': false,
+      'coursePercentage': 20.8,
+      'buyDate': '1969-07-20T20:18:04.000Z',
+      'courseTag': ['New'],
+      'courseDetails': {
+        'teacherName': 'Tayler Mostoult',
+        'totalSubscribers': 5402,
+      },
+    },
+    {
+      'title': 'Micro-frontends in flutter',
+      'finished': true,
+      'coursePercentage': 100.0,
+      'buyDate': '1969-07-20T20:18:04.000Z',
+      'courseTag': [],
+      'courseDetails': {},
+    },
+  ]
+};
+
+final user = User(
+  userName: 'Igor Miranda Souza',
+  age: 22,
+  totalCash: 138.25,
+  isMale: true,
+  userStatus: UserStatus(isUserActive: true, isAPremiumUser: false),
+  userTags: ['Flutter Developer', 'Proactive', 'Clean code'],
+  userCourses: [
+    UserCourse(
+      title: 'How to use TDD in flutter',
+      finished: false,
+      coursePercentage: 47.2,
+      buyDate: '1969-07-20T20:18:04.000Z',
+      courseTag: ['New', 'Popular'],
+      courseDetails: CourseDetails(
+        teacherName: 'Tayler Mostoult',
+        totalSubscribers: 5402,
+      ),
+    ),
+    UserCourse(
+      title: 'Clean arquiteture in flutter',
+      finished: true,
+      coursePercentage: 100.0,
+      buyDate: '1969-07-20T20:18:04.000Z',
+      courseTag: ['New'],
+      courseDetails: CourseDetails(
+        teacherName: 'Max Daniel',
+        totalSubscribers: 5402,
+      ),
+    ),
+  ],
+);
+
+class User {
+  final String userName;
+  final int age;
+  final double totalCash;
+  final bool isMale;
+  final UserStatus userStatus;
+  final List<String> userTags;
+  final List<UserCourse> userCourses;
+  const User({
+    required this.userName,
+    required this.age,
+    required this.totalCash,
+    required this.isMale,
+    required this.userStatus,
+    required this.userTags,
+    required this.userCourses,
+  });
+
+  @override
+  String toString() {
+    return 'User(userName: $userName, age: $age, totalCash: $totalCash, isMale: $isMale, userStatus: $userStatus, userTags: $userTags, userCourses: $userCourses)';
+  }
+}
+
+class UserStatus {
+  final bool isUserActive;
+  final bool isAPremiumUser;
+  const UserStatus({
+    required this.isUserActive,
+    required this.isAPremiumUser,
+  });
+
+  @override
+  String toString() =>
+      'UserStatus(isUserActive: $isUserActive, isAPremiumUser: $isAPremiumUser)';
+}
+
+class UserCourse {
+  final String title;
+  final bool finished;
+  final double coursePercentage;
+  final String buyDate;
+  final List<String> courseTag;
+  final CourseDetails courseDetails;
+  const UserCourse({
+    required this.title,
+    required this.finished,
+    required this.coursePercentage,
+    required this.buyDate,
+    required this.courseTag,
+    required this.courseDetails,
+  });
+
+  @override
+  String toString() {
+    return 'UserCourse(title: $title, finished: $finished, coursePercentage: $coursePercentage, buyDate: $buyDate, courseTag: $courseTag, courseDetails: $courseDetails)';
+  }
+}
+
+class CourseDetails {
+  final String teacherName;
+  final int totalSubscribers;
+  const CourseDetails({
+    required this.teacherName,
+    required this.totalSubscribers,
+  });
+
+  @override
+  String toString() =>
+      'CourseDetails(teacherName: $teacherName, totalSubscribers: $totalSubscribers)';
+}
+
+final testMap = {
   'userName': 'Igor',
   'age': 22,
   'totalCash': 138.57,
